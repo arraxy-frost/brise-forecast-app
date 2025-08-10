f8259b999c5380e2cc9b520493d3f31f71e83b8c
arraxy-frost <artyom.prilip@gmail.com>
2025-08-10 16:02:56 +0600

    chore(init): setup prettier, init fastify adapter, set global prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+    FastifyAdapter,
+    NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const PORT = 3000;
+
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+    );
+    app.setGlobalPrefix('api');
+
+    await app.listen(PORT, '0.0.0.0');
 }
+
 bootstrap();
